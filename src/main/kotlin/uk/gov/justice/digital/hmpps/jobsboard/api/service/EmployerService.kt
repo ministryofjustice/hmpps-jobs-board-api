3b6eb427391743dc8974dadb28ee1461435c2760
Javier Gómez <168651803+jgomezzmoj@users.noreply.github.com>
2024-07-11 15:41:51 +0100

    [ESWE-964] Adds the ability to return a list of all Employers (#30)
    The purpose of this pull request is to provide a very primitive
    capability of the `Employers` endpoint to return a list of all items
    stored in the database:
    - Adds the ability to the endpoint to return a simple list of
    `Employers`
    - Does some test refactor to use asserThat over `asserEquals` or
    `assertTrue`/`assertFalse`
    
    Out of scope:
    - Filtering
    - Pagination
    - Ordering

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/service/EmployerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/service/EmployerService.kt
@@ -1,9 +1,5 @@
 package uk.gov.justice.digital.hmpps.jobsboard.api.service
 
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.jobsboard.api.entity.Employer
 import uk.gov.justice.digital.hmpps.jobsboard.api.entity.EntityId
@@ -40,15 +36,7 @@ class EmployerService(
     return employerRepository.findById(EntityId(id)).orElseThrow { RuntimeException("Employer not found") }
   }
 
-  fun getPagingList(pageNo: Int, pageSize: Int, sortBy: String): MutableList<Employer>? {
-    val paging: Pageable = PageRequest.of(pageNo.toInt(), pageSize.toInt(), Sort.by(sortBy))
-
-    val pagedResult: Page<Employer> = employerRepository.findAll(paging)
-
-    if (pagedResult.hasContent()) {
-      return pagedResult.getContent()
-    } else {
-      return ArrayList<Employer>()
-    }
+  fun getEmployers(): List<Employer> {
+    return employerRepository.findAll()
   }
 }
