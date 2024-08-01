018fdb7bdf1ee604eed5c55045aa67cbe4451c6f
Javier Gómez <168651803+jgomezzmoj@users.noreply.github.com>
2024-08-01 08:05:42 +0100

    [ESWE-960] Job is created with valid UUID (#41)
    This PR:
    - Renames some Job tests to make them more expressive
    - Adds new tests to validate the Job UUID

--- src/test/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/jobs/application/JobCreatorShould.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/jobs/application/JobCreatorShould.kt
@@ -2,10 +2,13 @@ package uk.gov.justice.digital.hmpps.jobsboard.api.jobs.application
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.any
 import org.mockito.InjectMocks
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.jobsboard.api.entity.EntityId
@@ -98,5 +101,194 @@ class JobCreatorShould : TestBase() {
     val actualJob = jobCaptor.firstValue
 
     assertThat(actualJob).usingRecursiveComparison().isEqualTo(expectedJob)
+  }
+
+  @Test
+  fun `throw an exception when saving a Job with an invalid UUID`() {
+    whenever(employerRepository.findById(EntityId("eaf7e96e-e45f-461d-bbcb-fd4cedf0499c")))
+      .thenReturn(Optional.of(amazonEmployer))
+
+    val createJobRequest = CreateJobRequest.from(
+      id = "invalid-uuid",
+      employerId = "eaf7e96e-e45f-461d-bbcb-fd4cedf0499c",
+      jobTitle = "Forklift operator",
+      sector = "WAREHOUSING",
+      industrySector = "LOGISTICS",
+      numberOfVacancies = 2,
+      sourcePrimary = "PEL",
+      sourceSecondary = "",
+      charityName = "",
+      postCode = "LS12",
+      salaryFrom = 11.93f,
+      salaryTo = 15.90f,
+      salaryPeriod = "PER_HOUR",
+      additionalSalaryInformation = "",
+      isPayingAtLeastNationalMinimumWage = false,
+      workPattern = "FLEXIBLE_SHIFTS",
+      hoursPerWeek = "FULL_TIME",
+      contractType = "TEMPORARY",
+      baseLocation = "WORKPLACE",
+      essentialCriteria = "",
+      desirableCriteria = "",
+      description = """
+      What's on offer:
+
+      - 5 days over 7, 05:30 to 15:30
+      - Paid weekly
+      - Immediate starts available
+      - Full training provided
+      
+      Your duties will include:
+
+      - Manoeuvring forklifts safely in busy industrial environments
+      - Safely stacking and unstacking large quantities of goods onto shelves or pallets
+      - Moving goods from storage areas to loading areas for transport
+      - Unloading deliveries and safely relocating the goods to their designated storage areas
+      - Ensuring forklift driving areas are free from spills or obstructions
+      - Regularly checking forklift equipment for faults or damages
+      - Consolidating partial pallets for incoming goods
+      """.trimIndent(),
+      offenceExclusions = listOf("NONE", "DRIVING"),
+      isRollingOpportunity = false,
+      closingDate = LocalDate.of(2024, JULY, 20).toString(),
+      isOnlyForPrisonLeavers = true,
+      startDate = LocalDate.of(2024, JULY, 20).toString(),
+      howToApply = "",
+      supportingDocumentationRequired = listOf("CV", "DISCLOSURE_LETTER"),
+      supportingDocumentationDetails = "",
+    )
+
+    val exception = assertThrows<IllegalArgumentException> {
+      jobCreator.createOrUpdate(createJobRequest)
+    }
+
+    verify(jobRepository, never()).save(any(Job::class.java))
+    assertThat(exception.message).isEqualTo("Invalid UUID format: {${createJobRequest.id}}")
+  }
+
+  @Test
+  fun `throw exception when saving a Job with an empty UUID`() {
+    whenever(employerRepository.findById(EntityId("eaf7e96e-e45f-461d-bbcb-fd4cedf0499c")))
+      .thenReturn(Optional.of(amazonEmployer))
+
+    val createJobRequest = CreateJobRequest.from(
+      id = "",
+      employerId = "eaf7e96e-e45f-461d-bbcb-fd4cedf0499c",
+      jobTitle = "Forklift operator",
+      sector = "WAREHOUSING",
+      industrySector = "LOGISTICS",
+      numberOfVacancies = 2,
+      sourcePrimary = "PEL",
+      sourceSecondary = "",
+      charityName = "",
+      postCode = "LS12",
+      salaryFrom = 11.93f,
+      salaryTo = 15.90f,
+      salaryPeriod = "PER_HOUR",
+      additionalSalaryInformation = "",
+      isPayingAtLeastNationalMinimumWage = false,
+      workPattern = "FLEXIBLE_SHIFTS",
+      hoursPerWeek = "FULL_TIME",
+      contractType = "TEMPORARY",
+      baseLocation = "WORKPLACE",
+      essentialCriteria = "",
+      desirableCriteria = "",
+      description = """
+      What's on offer:
+
+      - 5 days over 7, 05:30 to 15:30
+      - Paid weekly
+      - Immediate starts available
+      - Full training provided
+      
+      Your duties will include:
+
+      - Manoeuvring forklifts safely in busy industrial environments
+      - Safely stacking and unstacking large quantities of goods onto shelves or pallets
+      - Moving goods from storage areas to loading areas for transport
+      - Unloading deliveries and safely relocating the goods to their designated storage areas
+      - Ensuring forklift driving areas are free from spills or obstructions
+      - Regularly checking forklift equipment for faults or damages
+      - Consolidating partial pallets for incoming goods
+      """.trimIndent(),
+      offenceExclusions = listOf("NONE", "DRIVING"),
+      isRollingOpportunity = false,
+      closingDate = LocalDate.of(2024, JULY, 20).toString(),
+      isOnlyForPrisonLeavers = true,
+      startDate = LocalDate.of(2024, JULY, 20).toString(),
+      howToApply = "",
+      supportingDocumentationRequired = listOf("CV", "DISCLOSURE_LETTER"),
+      supportingDocumentationDetails = "",
+    )
+
+    val exception = assertThrows<IllegalArgumentException> {
+      jobCreator.createOrUpdate(createJobRequest)
+    }
+
+    verify(jobRepository, never()).save(any(Job::class.java))
+    assertThat(exception.message).isEqualTo("EntityId cannot be empty")
+  }
+
+  @Test
+  fun `throw an exception when saving a Job with a null UUID`() {
+    whenever(employerRepository.findById(EntityId("eaf7e96e-e45f-461d-bbcb-fd4cedf0499c")))
+      .thenReturn(Optional.of(amazonEmployer))
+
+    val createJobRequest = CreateJobRequest.from(
+      id = "00000000-0000-0000-0000-00000",
+      employerId = "eaf7e96e-e45f-461d-bbcb-fd4cedf0499c",
+      jobTitle = "Forklift operator",
+      sector = "WAREHOUSING",
+      industrySector = "LOGISTICS",
+      numberOfVacancies = 2,
+      sourcePrimary = "PEL",
+      sourceSecondary = "",
+      charityName = "",
+      postCode = "LS12",
+      salaryFrom = 11.93f,
+      salaryTo = 15.90f,
+      salaryPeriod = "PER_HOUR",
+      additionalSalaryInformation = "",
+      isPayingAtLeastNationalMinimumWage = false,
+      workPattern = "FLEXIBLE_SHIFTS",
+      hoursPerWeek = "FULL_TIME",
+      contractType = "TEMPORARY",
+      baseLocation = "WORKPLACE",
+      essentialCriteria = "",
+      desirableCriteria = "",
+      description = """
+      What's on offer:
+
+      - 5 days over 7, 05:30 to 15:30
+      - Paid weekly
+      - Immediate starts available
+      - Full training provided
+      
+      Your duties will include:
+
+      - Manoeuvring forklifts safely in busy industrial environments
+      - Safely stacking and unstacking large quantities of goods onto shelves or pallets
+      - Moving goods from storage areas to loading areas for transport
+      - Unloading deliveries and safely relocating the goods to their designated storage areas
+      - Ensuring forklift driving areas are free from spills or obstructions
+      - Regularly checking forklift equipment for faults or damages
+      - Consolidating partial pallets for incoming goods
+      """.trimIndent(),
+      offenceExclusions = listOf("NONE", "DRIVING"),
+      isRollingOpportunity = false,
+      closingDate = LocalDate.of(2024, JULY, 20).toString(),
+      isOnlyForPrisonLeavers = true,
+      startDate = LocalDate.of(2024, JULY, 20).toString(),
+      howToApply = "",
+      supportingDocumentationRequired = listOf("CV", "DISCLOSURE_LETTER"),
+      supportingDocumentationDetails = "",
+    )
+
+    val exception = assertThrows<IllegalArgumentException> {
+      jobCreator.createOrUpdate(createJobRequest)
+    }
+
+    verify(jobRepository, never()).save(any(Job::class.java))
+    assertThat(exception.message).isEqualTo("EntityId cannot be null: {${createJobRequest.id}}")
   }
 }
