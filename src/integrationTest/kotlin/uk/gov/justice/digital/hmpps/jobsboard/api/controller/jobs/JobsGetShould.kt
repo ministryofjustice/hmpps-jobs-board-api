0480e6fb2c801b68a63fa9a7ba2f16aca49a9c34
Javier Gomez <javier.gomez@digital.justice.gov.uk>
2024-08-15 18:01:19 +0100

    [RED] Should retrieve a default paginated Jobs list filtered by incomplete Employer name

--- src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/controller/jobs/JobsGetShould.kt
+++ src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/controller/jobs/JobsGetShould.kt
@@ -108,6 +108,18 @@ class JobsGetShould : JobsTestCase() {
     )
   }
 
+  @Test
+  fun `retrieve a default paginated Jobs list filtered by incomplete Employer name`() {
+    givenTwoJobsAreRegistered()
+
+    assertGetJobIsOK(
+      parameters = "jobTitleOrEmployerName=Tes",
+      expectedResponse = expectedResponseListOf(
+        tescoWarehouseHandlerJobResponse(jobCreationTime),
+      ),
+    )
+  }
+
   private fun givenTwoJobsAreRegistered() {
     assertAddEmployer(
       id = "89de6c84-3372-4546-bbc1-9d1dc9ceb354",
