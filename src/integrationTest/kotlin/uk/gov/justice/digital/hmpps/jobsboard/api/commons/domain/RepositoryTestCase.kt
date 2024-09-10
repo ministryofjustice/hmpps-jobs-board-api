78a6b8d9084aecd144164e8483e39b719a5a67a4
Rick Choi <rick.choi@digital.justice.gov.uk>
2024-09-10 14:38:56 +0100

    [ESWE-1051] Job Archived DB, domain classes (entity and repository) (#66)
    1. DB table for Job Archived
    2. Domain entity and repository classes for Job Archived
    3. Archived member of Job entity
    4. Repository tests for Job Archived

---
+++ src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/commons/domain/RepositoryTestCase.kt
@@ -0,0 +1,56 @@
+package uk.gov.justice.digital.hmpps.jobsboard.api.commons.domain
+
+import jakarta.persistence.EntityManager
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInstance
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Import
+import org.springframework.data.auditing.DateTimeProvider
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.jobsboard.api.config.TestJpaConfig
+import uk.gov.justice.digital.hmpps.jobsboard.api.jobs.domain.TestPrototypes
+import uk.gov.justice.digital.hmpps.jobsboard.api.testcontainers.PostgresContainer
+import java.time.Instant
+import java.util.*
+
+@DataJpaTest
+@Import(TestJpaConfig::class)
+@AutoConfigureTestDatabase(replace = NONE)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test-containers")
+@Transactional
+abstract class RepositoryTestCase {
+  @Autowired
+  protected lateinit var dateTimeProvider: DateTimeProvider
+
+  @Autowired
+  protected lateinit var entityManager: EntityManager
+
+  protected final val defaultCreationTime: Instant = TestPrototypes.defaultCreationTime
+
+  @BeforeEach
+  fun setUp() {
+    whenever(dateTimeProvider.now).thenReturn(Optional.of(defaultCreationTime))
+  }
+
+  companion object {
+    private val postgresContainer = PostgresContainer.repositoryContainer
+
+    @JvmStatic
+    @DynamicPropertySource
+    fun configureTestContainers(registry: DynamicPropertyRegistry) {
+      postgresContainer?.run {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl)
+        registry.add("spring.datasource.username", postgresContainer::getUsername)
+        registry.add("spring.datasource.password", postgresContainer::getPassword)
+      }
+    }
+  }
+}
---
+++ src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/jobs/domain/ArchivedRepositoryShould.kt
@@ -0,0 +1,125 @@
+package uk.gov.justice.digital.hmpps.jobsboard.api.jobs.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.DataAccessException
+import java.time.Duration
+import java.util.*
+
+class ArchivedRepositoryShould : JobRepositoryTestCase() {
+
+  @Autowired
+  private lateinit var archivedRepository: ArchivedRepository
+
+  private val jobArchivedTime = jobCreationTime.plus(Duration.ofDays(1))
+  private val jobRedoArchivedTime = jobCreationTime.plus(Duration.ofDays(2))
+
+  @Test
+  fun `archive a job for given prisoner`() {
+    val job = obtainTheJobJustCreated()
+
+    val savedJobArchived = makeJobArchived(job, expectedPrisonNumber).let { jobArchived ->
+      archivedRepository.saveAndFlush(jobArchived)
+    }
+
+    val jobArchivedFreshCopy =
+      archivedRepository.findById(savedJobArchived.id).orElseThrow()
+    assertThat(jobArchivedFreshCopy).usingRecursiveComparison().isEqualTo(savedJobArchived)
+  }
+
+  @Test
+  fun `set createdAt attribute, when archiving job`() {
+    val job = obtainTheJobJustCreated()
+
+    val jobArchived = makeJobArchived(job, expectedPrisonNumber)
+    whenever(dateTimeProvider.now).thenReturn(Optional.of(jobArchivedTime))
+    val savedJobArchived = archivedRepository.saveAndFlush(jobArchived)
+
+    assertThat(savedJobArchived.createdAt).isEqualTo(jobArchivedTime)
+  }
+
+  @Test
+  fun `do NOT update job's attribute, when archiving job`() {
+    val job = obtainTheJobJustCreated()
+
+    whenever(dateTimeProvider.now).thenReturn(Optional.of(jobArchivedTime))
+    val savedJobArchived = makeJobArchived(job, expectedPrisonNumber).let { jobArchived ->
+      archivedRepository.saveAndFlush(jobArchived)
+    }
+
+    val jobFreshCopy = jobRepository.findById(savedJobArchived.job.id).orElseThrow()
+    assertThat(jobFreshCopy).usingRecursiveComparison().ignoringFields("expressionsOfInterest").isEqualTo(job)
+    assertThat(jobFreshCopy.modifiedAt).isEqualTo(jobCreationTime)
+  }
+
+  @Test
+  fun `do NOT archive again, when it exists`() {
+    val job = obtainTheJobJustCreated()
+    whenever(dateTimeProvider.now).thenReturn(Optional.of(jobArchivedTime))
+    val savedJobArchived = makeJobArchived(job, expectedPrisonNumber).let { jobArchived ->
+      archivedRepository.saveAndFlush(jobArchived)
+    }
+
+    val duplicateArchived = makeJobArchived(
+      job = savedJobArchived.job,
+      prisonNumber = savedJobArchived.id.prisonNumber,
+    )
+    whenever(dateTimeProvider.now).thenReturn(Optional.of(jobRedoArchivedTime))
+    val updatedArchived = archivedRepository.saveAndFlush(duplicateArchived).also {
+      entityManager.refresh(it)
+    }
+
+    assertThat(updatedArchived).usingRecursiveComparison().isEqualTo(savedJobArchived)
+    assertThat(updatedArchived.createdAt).isEqualTo(jobArchivedTime)
+  }
+
+  @Test
+  fun `throw exception, when archiving a non-existent job`() {
+    val jobId = nonExistentJob.id.toString()
+    val jobArchived = makeJobArchived(nonExistentJob, expectedPrisonNumber)
+    whenever(dateTimeProvider.now).thenReturn(Optional.of(jobArchivedTime))
+
+    val exception = assertThrows<Exception> {
+      archivedRepository.save(jobArchived)
+    }
+
+    assertThat(exception).isInstanceOf(DataAccessException::class.java)
+    assertThat(exception.message)
+      .contains("Unable to find")
+      .contains(jobId)
+  }
+
+  @Test
+  fun `undo archiving a job for given prisoner`() {
+    val job = obtainTheJobJustCreated()
+    whenever(dateTimeProvider.now).thenReturn(Optional.of(jobArchivedTime))
+    val savedJobArchived = makeJobArchived(job, expectedPrisonNumber).let { jobArchived ->
+      archivedRepository.saveAndFlush(jobArchived)
+    }
+
+    archivedRepository.deleteById(savedJobArchived.id)
+
+    val searchArchived = archivedRepository.findById(savedJobArchived.id)
+    assertThat(searchArchived.isEmpty).isTrue()
+  }
+
+  @Test
+  fun `do NOT update job's attribute, when undo archiving`() {
+    val job = obtainTheJobJustCreated()
+    whenever(dateTimeProvider.now).thenReturn(Optional.of(jobArchivedTime))
+    val savedJobArchived = makeJobArchived(job, expectedPrisonNumber).let { jobArchived ->
+      archivedRepository.saveAndFlush(jobArchived)
+    }
+
+    archivedRepository.deleteById(savedJobArchived.id)
+
+    val searchJob = jobRepository.findById(job.id).orElseThrow()
+    assertThat(searchJob).usingRecursiveComparison().isEqualTo(job)
+  }
+
+  private fun makeJobArchived(job: Job, prisonNumber: String): Archived =
+    Archived(id = JobPrisonerId(job.id, prisonNumber), job = job)
+}
