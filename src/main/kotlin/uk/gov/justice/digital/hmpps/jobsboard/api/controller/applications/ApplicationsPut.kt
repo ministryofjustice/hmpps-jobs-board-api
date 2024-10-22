484950ea3cce84762068eda31f6743a53c017b05
rickchoijd <rick.choi@digital.justice.gov.uk>
2024-10-22 21:24:52 +0100

    [GREEN] add applications to Job
    add back missing OneToMany at Job (to Application);
    fix broken tests
    update API docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/controller/applications/ApplicationsPut.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/controller/applications/ApplicationsPut.kt
@@ -1,5 +1,10 @@
 package uk.gov.justice.digital.hmpps.jobsboard.api.controller.applications
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Pattern
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import uk.gov.justice.digital.hmpps.jobsboard.api.applications.application.ApplicationCreator
 import uk.gov.justice.digital.hmpps.jobsboard.api.applications.application.CreateApplicationRequest
+import uk.gov.justice.digital.hmpps.jobsboard.api.config.ErrorResponse
 
 @Validated
 @RestController
@@ -23,12 +29,43 @@ class ApplicationsPut(
 ) {
   @PreAuthorize("hasRole('ROLE_EDUCATION_WORK_PLAN_EDIT')")
   @PutMapping("/{id}")
+  @Operation(
+    summary = "Create or Update an Application to a job for the prisoner",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "The success status is set as the creation request has been processed correctly.",
+        content = [Content()],
+      ),
+      ApiResponse(
+        responseCode = "201",
+        description = "The success status is set as the update request has been processed correctly.",
+        content = [Content()],
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "The failure status is set when the request is invalid. An error response will be provided.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content()],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Incorrect permissions to access this endpoint",
+        content = [Content()],
+      ),
+    ],
+  )
   fun createOrUpdate(
     @PathVariable
     @Pattern(
       regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
       message = "Invalid UUID format",
     )
+    @Parameter(description = "Application ID, identifier of the application to the given job")
     id: String,
     @Valid @RequestBody createApplicationRequest: CreateApplicationRequest,
   ): ResponseEntity<Void> {
