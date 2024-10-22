f32bb33de9625335cf83aa27820664c11278e44e
Javier Gomez <javier.gomez@digital.justice.gov.uk>
2024-10-22 09:34:29 +0100

    [GREEN] Postcode location service saves a postcode with coordinates

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/jobs/application/PostcodeLocationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/jobs/application/PostcodeLocationService.kt
@@ -4,18 +4,23 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.jobsboard.api.entity.EntityId
 import uk.gov.justice.digital.hmpps.jobsboard.api.jobs.domain.Postcode
 import uk.gov.justice.digital.hmpps.jobsboard.api.jobs.domain.PostcodesRepository
+import uk.gov.justice.digital.hmpps.jobsboard.api.jobs.infrastructure.OSPlacesAPIClient
 
 @Service
 class PostcodeLocationService(
   private val postcodesRepository: PostcodesRepository,
+  private val osPlacesAPIClient: OSPlacesAPIClient,
+  private val uuidGenerator: UUIDGenerator,
 ) {
   fun save(postcode: String) {
+    val postcodeCoordinates = osPlacesAPIClient.getAddressesFor(postcode)
+
     postcodesRepository.save(
       Postcode(
-        id = EntityId(UUIDGenerator()),
+        id = EntityId(uuidGenerator),
         code = postcode,
-        xCoordinate = 0.00,
-        yCoordinate = 0.00,
+        xCoordinate = postcodeCoordinates.xCoordinate,
+        yCoordinate = postcodeCoordinates.yCoordinate,
       ),
     )
   }
