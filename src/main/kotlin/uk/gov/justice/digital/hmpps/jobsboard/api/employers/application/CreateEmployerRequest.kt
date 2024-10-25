ca28e4b0186bdc1f4c32c1fd239ab7e4a75c7fde
rickchoijd <rick.choi@digital.justice.gov.uk>
2024-10-25 13:59:54 +0100

    [GREEN] Validate the length of Employer 's description

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/employers/application/CreateEmployerRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/employers/application/CreateEmployerRequest.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.jobsboard.api.employers.application
 
+import jakarta.validation.constraints.Size
+
 @ConsistentCopyVisibility
 data class CreateEmployerRequest internal constructor(
   val id: String = "",
   val name: String,
+  @field:Size(max = 500)
   val description: String,
   val sector: String,
   val status: String,
