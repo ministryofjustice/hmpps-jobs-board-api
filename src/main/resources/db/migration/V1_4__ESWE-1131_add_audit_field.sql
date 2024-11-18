04c34604bb76cd44ac95f0b3331930a9b03163d6
rickchoijd <rick.choi@digital.justice.gov.uk>
2024-11-18 12:16:13 +0000

    [GREEN] Fix Flyway migration error of new columns

--- src/main/resources/db/migration/V1_4__ESWE-1131_add_audit_field.sql
+++ src/main/resources/db/migration/V1_4__ESWE-1131_add_audit_field.sql
@@ -1,10 +1,7 @@
 ALTER TABLE jobs_expressions_of_interest
-    ADD COLUMN created_by VARCHAR(240);
+    ADD COLUMN IF NOT EXISTS created_by VARCHAR(240) NOT NULL DEFAULT 'system';
 ALTER TABLE jobs_archived
-    ADD COLUMN created_by VARCHAR(240);
+    ADD COLUMN IF NOT EXISTS created_by VARCHAR(240) NOT NULL DEFAULT 'system';
 
-UPDATE jobs_expressions_of_interest set created_by = 'system' WHERE created_by is NULL;
-UPDATE jobs_archived set created_by = 'system' WHERE created_by is NULL;
-
-ALTER TABLE jobs_expressions_of_interest ALTER COLUMN created_by SET NOT NULL;
-ALTER TABLE jobs_archived ALTER COLUMN created_by SET NOT NULL;
+ALTER TABLE jobs_expressions_of_interest ALTER COLUMN created_by DROP DEFAULT;
+ALTER TABLE jobs_archived ALTER COLUMN created_by DROP DEFAULT;
