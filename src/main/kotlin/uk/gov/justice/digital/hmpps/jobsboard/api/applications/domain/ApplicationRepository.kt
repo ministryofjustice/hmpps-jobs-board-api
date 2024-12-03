f6566b84021650319787ae0b3984b126fe7d970b
rickchoijd <rick.choi@digital.justice.gov.uk>
2024-12-03 14:52:36 +0000

    [RED] Add Reporting metrics of dashboard - Latest applications by application status (repository)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/applications/domain/ApplicationRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/applications/domain/ApplicationRepository.kt
@@ -87,4 +87,46 @@ interface ApplicationRepository :
     startTime: Instant,
     endTime: Instant,
   ): List<MetricsCountByStatus>
+
+  @Query(
+    """
+    WITH recent_updates AS (
+        SELECT rev_number, id, status
+        FROM applications_audit aa
+        WHERE aa.last_modified_at BETWEEN :startTime AND :endTime AND aa.prison_id = :prisonId
+    ),
+    status_at_start AS (
+      SELECT aa1.rev_number, aa1.id, aa1.status FROM applications_audit aa1
+      INNER JOIN (
+        SELECT id, max(rev_number) as rev_number 
+        FROM applications_audit aa2
+        WHERE aa2.id IN (SELECT DISTINCT id from recent_updates) AND aa2.last_modified_at <= :startTime
+        GROUP BY id
+      ) at_start 
+      ON aa1.id = at_start.id AND aa1.rev_number = at_start.rev_number
+      AND aa1.rev_number NOT IN (SELECT rev_number from recent_updates)
+    ),
+    application_stages AS (
+        SELECT * FROM recent_updates UNION ALL SELECT * FROM status_at_start
+    ),
+    open_applications AS (
+        SELECT DISTINCT id FROM application_stages WHERE UPPER(status) IN ('APPLICATION_MADE', 'SELECTED_FOR_INTERVIEW', 'INTERVIEW_BOOKED')
+    ),
+    application_latest AS (
+        SELECT stages.* FROM application_stages stages 
+        INNER JOIN (SELECT id, MAX(rev_number) as rev_number FROM application_stages GROUP BY id) latest_rev 
+        ON stages.id = latest_rev.id AND stages.rev_number = latest_rev.rev_number 
+        WHERE stages.id IN (SELECT id FROM open_applications) 
+    )
+    SELECT status, COUNT(DISTINCT id) as count
+    FROM application_latest 
+    GROUP BY status 
+    """,
+    nativeQuery = true,
+  )
+  fun countApplicationStatusByPrisonIdAndDateTimeBetween(
+    prisonId: String,
+    startTime: Instant,
+    endTime: Instant,
+  ): List<MetricsCountByStatus>
 }
