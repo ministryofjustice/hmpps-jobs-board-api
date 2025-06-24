3388c88d763f26f413ff8f6bfe0fff213f9035a4
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-06-24 14:41:41 +0100

    [ESWE-1345] Upgrade dependencies
    Upgrade:
    * SQS lib `5.4.6` (was `5.4.5`)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.6")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
