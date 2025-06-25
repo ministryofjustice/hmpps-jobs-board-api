78c3b9ae0154cb68c1194b27ee840af1b6752792
Rick Choi <rick.choi@justice.gov.uk>
2025-06-25 10:24:30 +0100

    [ESWE-1345] Upgrade dependencies (#175)
    Upgrade:
    * Spring Boot to `3.5.3` (was `3.5.0`)
    * `gradle-spring-boot` `8.3.0` (was `8.2.0`)
    * SQS lib `5.4.6` (was `5.4.5`)
    * Gradle wrapper `8.14.2` (was `8.14.1`)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
   kotlin("plugin.spring") version "2.1.21"
   kotlin("plugin.jpa") version "2.1.21"
   id("jvm-test-suite")
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.6")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
