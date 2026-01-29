fa45f7711fb4351aa24d49da97a51e4a835111e7
rickchoijd <rick.choi@justice.gov.uk>
2026-01-29 18:43:58 +0000

    ESWE-1667: Tidy up codebase; remove unused codes
    Remove unused codes in these packages
    - `uk.gov.justice.digital.hmpps.jobsboard.api.messaging`
    - `uk.gov.justice.digital.hmpps.jobsboard.api.repository`
    - `uk.gov.justice.digital.hmpps.jobsboard.api.telemetry`
    - `uk.gov.justice.digital.hmpps.jobsboard.data.jsonprofile`
    - `uk.gov.justice.digital.hmpps.jobsboard.api.entity`  (except `EntityId`)
    
    Remove these classes from
    `uk.gov.justice.digital.hmpps.jobsboard.api.exceptions`
    - `AlreadyExistsException`
    - `ExceptionHandler`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/exceptions/CommonExceptions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboard/api/exceptions/CommonExceptions.kt
@@ -1,16 +1,6 @@
 package uk.gov.justice.digital.hmpps.jobsboard.api.exceptions
 
-import org.slf4j.LoggerFactory
 import java.util.function.Supplier
-
-class ExceptionHandler {
-
-  companion object {
-    private val log = LoggerFactory.getLogger(this::class.java)
-  }
-}
-
-class AlreadyExistsException(offenderId: String) : Exception("CM profile already exists for offender $offenderId")
 
 class NotFoundException(var offenderId: String) :
   Exception("CM profile does not exist for offender $offenderId"),
